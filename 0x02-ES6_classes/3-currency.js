a62cea79d169c9be882eea748b5af7d9233c3da0
Ian Kisali <iankisali@gmail.com>
2023-04-21 12:15:41 +0300

    class Currency return attributes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof (code) !== 'string') {
+      throw new TypeError('Code must be s string');
+    }
+    this._code = code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
