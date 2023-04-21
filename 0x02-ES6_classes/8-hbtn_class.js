0b7d55642e520e9eca70534d483f22d72a4418e5
Ian Kisali <iankisali@gmail.com>
2023-04-21 14:12:32 +0300

    class HolbertonClass with casting

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,21 +1,21 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    get size() {
-        return this._size;
-    }
+  get size() {
+    return this._size;
+  }
 
-    get location() {
-        return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 
-    [Symbol.toPrimitive](type) {
-        if (type === 'string') {
-            return this.location;
-        }
-        return this.size;
+  [Symbol.toPrimitive](type) {
+    if (type === 'string') {
+      return this.location;
     }
+    return this.size;
+  }
 }
