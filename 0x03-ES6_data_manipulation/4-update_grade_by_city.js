53ba3af759c15a81abe783ab0bcc7f31fd0076bc
Ian Kisali <iankisali@gmail.com>
2023-04-24 18:42:20 +0300

    returns array of students for specific city with new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-        .filter(({ location }) => location === city)
-        .map((student) => {
-            const newGrade = newGrades.find(({ studentId }) => studentId === student.id);
-            return {
-                ...student,
-                grade: newGrade ? newGrade.grade : 'N/A',
-            };
-        });
+  return students
+    .filter(({ location }) => location === city)
+    .map((student) => {
+      const newGrade = newGrades.find(({ studentId }) => studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
