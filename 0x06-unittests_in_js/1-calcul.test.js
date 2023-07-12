c3fcf6ca4a76d203a68dd4714c188d284291942d
Ian Kisali <iankisali@gmail.com>
2023-07-12 11:07:26 +0300

    combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+const { it, describe } = require('mocha')
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+    it('Checking sum', () => {
+        assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+    it('Checking subtraction', () => {
+        assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('Cheking division', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('Checking division by zero', () => {
+        assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error;');
+    });
+});
