0c8383204062a6bd4d357d12c92233dac0fe1d91
Ian Kisali <iankisali@gmail.com>
2023-07-22 16:28:21 +0300

    organizing the structure of the server

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(Error(error));
+        return;
+      }
+      const file_data = data.toString().split('\n');
+      let student = file_data.filter((item) => item);
+      student = student.map((item) => item.split(','));
+      const fields = {};
+
+      for (const i in student) {
+        if (i !== 0) {
+          if (!fields[student[i][3]]) fields[student[i][3]] = [];
+          fields[student[i][3]].push(student[i][0]);
+        }
+      }
+      delete fields.field;
+      resolve(fields);
+    });
+  });
+}
+
+export default readDatabase;
